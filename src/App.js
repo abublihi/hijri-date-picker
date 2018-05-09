1a443572cb5f75e5dcff8a3f9eed18d7dce50665
Abdulsalam salh alblihi <abublihi@gmail.com>
2018-05-09 07:07:32 +0300

    add DayNames component

--- src/App.js
+++ src/App.js
@@ -25,7 +25,10 @@ class HijriDatePicker extends Component {
     time: moment(),
     monthDays: moment().iDaysInMonth(),
     selectedDate: '',
+    arabicDayNames: ['احد', 'اثنين', 'ثلاثاء', 'اربعاء', 'خميس', 'جمعة'],
+    englishDayNames: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
   }
+
   monthDays = () => {
     return this.state.time.iDaysInMonth()
   }
@@ -34,7 +37,12 @@ class HijriDatePicker extends Component {
     this.setState({
       time: this.state.time.subtract(1, 'iMonth')
     })
-    console.log(this.state.time.format('iYYYY/iM/iD'))
+  }
+
+  addMonth = () => {
+    this.setState({
+      time: this.state.time.add(1, 'iMonth')
+    })
   }
 
   setSelectedDate = (event) => {
@@ -43,26 +51,65 @@ class HijriDatePicker extends Component {
     this.setState({
       selectedDate: time.format('iYYYY/iMM/iDD')
     })
+
+    // // m = moment();
+    // time.startOf('iMonth');
+    // console.log(time.format('ddd'));
   }
+
+  getMonthStartDayName = () => {
+    let time = this.state.time
+    time.startOf('iMonth')
+    return time.format('dd') 
+
+  }
+
   render() {
+    console.log(this.getMonthStartDayName())
     let daysList = []
-    for (let i = 1; i < this.monthDays() + 1; i++) {
+    for (let i = this.state.englishDayNames.indexOf(this.getMonthStartDayName()); i > 0; i--){
       daysList.push(
-        <li><button onClick={this.setSelectedDate} value={i}>{i}</button></li>
+        <div className="month-day"></div>
       )
     }
+    for (let i = 1; i < this.monthDays() + 1; i++) {
+      daysList.push(
+        <div className="month-day"><button onClick={this.setSelectedDate} value={i}>{i}</button></div>
+      )
+    }
+
     return (
       <div className="hijri-date-picker">
         <input type="text" value={this.state.selectedDate} />
         <div className="hijri-calender">
-          <h4 className="month-name">شعبان</h4>
+          <strong className="month-name">{this.state.time.format('iMMMM') + ' ' + this.state.time.format('iYYYY')}</strong>
+          <DayNames />
           <button onClick={this.subtractMonth} >></button>
+          <button onClick={this.addMonth} > {'<'} </button>
           <div className="month-days">
             {
               daysList
             }
           </div>
         </div>
+      </div>
+    )
+  }
+}
+
+class DayNames extends Component {
+  state = {
+    arabicDayNames: ['ح', 'ن', 'ث', 'ر', 'خ', 'ج', 'س'],
+    arabicFullDayNames: ['احد', 'اثنين', 'ثلاثاء', 'اربعاء', 'خميس', 'جمعة', 'سبت'],
+    englishDayNames: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+  }
+  
+  render() {
+    return (
+      <div className="day-names">
+        {
+          this.state.arabicDayNames.map((item, key) =>  <div className="day-name" key={key.toString()}>{item}</div>)
+        }
       </div>
     )
   }
