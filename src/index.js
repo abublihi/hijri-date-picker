4d9d4d7460f16486475aa621e08d8f9878825cfc
abdulsalam <abds.alblihi@qu.edu.sa>
2018-05-09 14:33:57 +0300

    load HijriDatePicker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import HijriDatePicker from './HijriDatePicker';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HijriDatePicker />, document.getElementById('root'));
 registerServiceWorker();
