01aa7558d11e5e2acf7e7a2886f4d770d2bed5f0
Abdulsalam salh alblihi <abublihi@gmail.com>
2018-05-14 22:25:12 +0300

    add selecteDate props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import HijriDatePicker from './HijriDatePicker';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<HijriDatePicker inputName="hijri_date" className="sdfs" />, document.getElementById('root'));
+ReactDOM.render(<HijriDatePicker inputName="hijri_date" className="form-control" selectedDate="1439/08/02" />, document.getElementById('root'));
 registerServiceWorker();
