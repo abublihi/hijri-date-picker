7ac8d22cf71297cdeac4d4843399f5b1759bc877
abdulsalam <abds.alblihi@qu.edu.sa>
2018-05-29 12:09:10 +0300

    a sample of how to export to web component using reactive-elements package

---
+++ src/HijriDatePickerElement.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HijriDatePicker from './HijriDatePicker';
+import document from 'reactive-elements';
+
+document.registerReact('hijri-date-picker', HijriDatePicker);
