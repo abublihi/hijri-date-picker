4f07b33b7f2951da5b868c807443db90a4b15b5f
AbdelrahmanAbdelrazek <abdelrahman.abdelrazek90@gmail.com>
2019-02-18 15:30:18 +0200

    add onFocus onChange events
    To treat the HijriDatePicker just like any normal `<input>` field, I added the onFocus and onChange events.
    Also added the ability for the component to be:
    	- Fully controlled by setting the `selectedDate` attribute.
    	- or Uncontrolled by not setting the `selectedDate` attribute.
    
    Resolves: #7

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import HijirDatePicker from '../lib';
 import './ExampleStyle.css';
 
-const App = () => (
-  <div id="app">
-    <HijirDatePicker inputName="hijri_date" className="input-style" selectedDate="08/1439/02" dateFormat="iMM/iYYYY/iDD" quickSelect/>
-  </div>
-);
+class App extends Component {
+  state = {}
+
+  onChange = (value) => {
+    console.log('OnChange -> Value is: ', value)
+    this.setState({ selectedDate: value })
+  }
+
+  onFocus = (value) => {
+    console.log('OnFocus -> Value is: ', value)
+  }
+
+  render() {
+    return (
+      <div id="app">
+        <HijirDatePicker
+          name="hijri_date"
+          className="input-style"
+          placeholder="Hijri Date"
+          selectedDate={this.state.selectedDate} //not required if it is uncontrolled component
+          dateFormat="iMM/iYYYY/iDD"
+          onChange={this.onChange}
+          onFocus={this.onFocus}
+          quickSelect
+        />
+      </div>
+    );
+  }
+}
 
 export default App;
